496a62499631beaeefc30619541bfcadcc827f4c
sofiapetruk <sofiapetruk900@gmail.com>
2024-04-10 20:07:56 -0300

    feito o exemplo 1

---
+++ pesquisa_binaria/ex1.1.py
@@ -0,0 +1,11 @@
+'''Suponha que você tenha uma lista com 128 nomes e esteja fazendo uma  pesquisa binária. 
+Qual seria o número máximo de etapas que você levaria  para encontrar o nome desejado? '''
+# Precisamos dividir o "128 / 2" que vai dar igual "64" depois / 2 novamente = "32" / 2 = "16"
+# vai ser assim até ficar o número 1, que irá ser o nosso número.
+# Para saber se é maior ou menos para eu dividir 64 preciso usar o 
+'''if chute == item: # 4
+            return meio
+        if chute > item: # 5
+            alto = meio - 1
+        else: # 6
+            baixo = meio + 1'''
