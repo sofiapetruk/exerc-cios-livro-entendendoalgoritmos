81ea91d27ab1f3858b05500a653b5a1f676004f5
sofiapetruk <sofiapetruk900@gmail.com>
2024-04-10 20:11:43 -0300

    ex 1 e 2

--- pesquisa_binaria/ex1.1.py
+++ pesquisa_binaria/ex1.1.py
@@ -2,7 +2,7 @@
 Qual seria o número máximo de etapas que você levaria  para encontrar o nome desejado? '''
 # Precisamos dividir o "128 / 2" que vai dar igual "64" depois / 2 novamente = "32" / 2 = "16"
 # vai ser assim até ficar o número 1, que irá ser o nosso número.
-# Para saber se é maior ou menos para eu dividir 64 preciso usar o 
+# Para saber se é maior ou menos para eu dividir 64 preciso usar o. Foram 7 etapas
 '''if chute == item: # 4
             return meio
         if chute > item: # 5
---
+++ pesquisa_binaria/ex1.2.py
@@ -0,0 +1,2 @@
+'''Suponha que você duplique o tamanho da lista. Qual seria o número máximo de etapas agora'''
+#Se dobrar o número para 256 ira que ser 8 etapas
